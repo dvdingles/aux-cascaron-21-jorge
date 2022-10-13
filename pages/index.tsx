fddc02fc50c8e244442977ef730fa835622beafa
dvdapogeo <dvdingles@hotmail.com>
2022-10-13 13:49:36 +0200

    six

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,7 +70,7 @@ const Home: NextPage = () => {
             
               <Dav 
                 // className={styles.dav}
-                scale={1}
+                scale={.7}
                 position={[0,0.15,0]}
                 rotation={[Math.PI / 1000, 0, 0]}
               />
